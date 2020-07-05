c9655e8d618f50f4a6c9e94f863246bcb882e5f7
vagrant <vagrant@homestead>
2020-07-05 07:37:16 +0000

    sitemap

--- resources/views/sitemap/article.blade.php
+++ resources/views/sitemap/article.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
    @foreach ($article as $article)
        <url>
-           <loc>http://letsengineering.tech/sitemap.xml/article/{{ $article->id }}</loc>
+           <loc>http://letsengineering.tech/article/{{ $article->id }}</loc>
            <lastmod>{{ $article->created_at->tz('UTC')->toAtomString() }}</lastmod>
            <changefreq>weekly</changefreq>
            <priority>0.9</priority>
--- resources/views/sitemap/category.blade.php
+++ resources/views/sitemap/category.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
    @foreach ($category as $category)
        <url>
-           <loc>http://letsengineering.tech/sitemap.xml/category/{{ $category->id }}</loc>
+           <loc>http://letsengineering.tech/category/{{ $category->id }}</loc>
            <lastmod>{{ $category->created_at->tz('UTC')->toAtomString() }}</lastmod>
            <changefreq>weekly</changefreq>
            <priority>0.9</priority>
